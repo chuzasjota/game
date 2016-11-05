2c752d3b423a4f017e51db933621afd2aef290b0
chuzasjota <chuzas.j@gmail.com>
2016-11-05 11:26:26 -0500

    Create pajaro.js Update ppal.js Space invader

--- js/ppal.js
+++ js/ppal.js
@@ -1,75 +1,79 @@
 var juego = new Phaser.Game(370,550,Phaser.AUTO,'bloquejuego');
 var fondoJuego, pajaro, boton, arcade;
-var cursores;
-var persona;
-var mirando = 'arriba';
+var nave, cursores;
+var balas, tiempoBala = 0, botonDisparo;
+var enemigos
+
+function colision(bala, enemigo){
+	bala.kill();
+	enemigo.kill();
+}
+
+
 
 var estadoPpal = {
 	preload: function(){
 		// Carga todos los recursos del juego
-		juego.load.image('fondo','img/bg.jpeg');
-		juego.load.spritesheet('pajaros','img/pajaro.png',43,30);
-		juego.load.image('btn','img/btn.png');
-		juego.load.spritesheet('persona','img/persona.png',64,64);
+		juego.load.image('fondo','img/space.png');
+		juego.load.image('personaje','img/nave.png');
+		juego.load.image('laser','img/laser.png');
+		juego.load.image('enemigo','img/pajaro2.png');
 	},
 	create: function(){
 		//Mostrar en pantalla todo lo cargado en preload
 		fondoJuego = juego.add.tileSprite(0,0,370, 550,'fondo');
-		//pajaro = juego.add.sprite(100,100,'pajaros');
-		//pajaro.frame = 1;
-		//pajaro.animations.add('vuelo',[0,1,2],10,true);
+		nave = juego.add.sprite(juego.width/2, 500, 'personaje');
+		nave.anchor.setTo(0.5);
 		cursores = juego.input.keyboard.createCursorKeys();
-		arcade = juego.physics.startSystem(Phaser.Physics.ARCADE);
-		//juego.physics.arcade.enable(pajaro);
-		//pajaro.body.collideWorldBounds = true;
-		persona = juego.add.sprite(juego.width/2,juego.height/2,'persona');
-		juego.physics.arcade.enable(persona);
-		persona.body.collideWorldBounds = true;
-		persona.anchor.setTo(0.5);
-		persona.animations.add('arriba', [0,1,2,3,4,5,6,7,8],10, true);
-		persona.animations.add('abajo', [18,19,20,21,22,23,24,25,26],10, true);
-		persona.animations.add('derecha', [27,28,29,30,31,32,33,34,35],10, true);
-		persona.animations.add('izquierda', [9,10,11,12,13,14,15,16,17],10, true);
+		botonDisparo = juego.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
+		balas = juego.add.group();
+		balas.enableBody = true;
+		balas.physicsBodyType = Phaser.Physics.ARCADE;
+		balas.createMultiple(20,'laser');
+		balas.setAll('anchor.x',0.5);
+		balas.setAll('anchor.y',1);
+		balas.setAll('outOfBoundsKill',true);
+		balas.setAll('checkWorldBounds',true);
+
+		enemigos = juego.add.group();
+		enemigos.enableBody = true;
+		enemigos.physicsBodyType = Phaser.Physics.ARCADE;
+		for(var y = 0; y < 6; y++) {
+			for (var x = 0; x < 7; x++) {
+				var enemigo = enemigos.create(x*40, y*20, 'enemigo');
+				enemigo.anchor.setTo(0.5);
+			}
+		}
+		enemigos.x = 50;
+		enemigos.y = 30;
+
+		function descender(){
+			enemigos.y += 10;
+		}
+
+		var animacion = juego.add.tween(enemigos).to({x:100},1000,Phaser.Easing.Linear.None, true, 0, 1000, true);
+		animacion.onLoop.add(descender, this);
 	},
 	update: function(){
-		//Animacion del juego
-		//fondoJuego.tilePosition.x -= 1;
-		//pajaro.animations.play('vuelo')
-		if(cursores.right.isDown){
-			//pajaro.position.x += 1;
-			persona.position.x += 1;
-			persona.animations.play('derecha');
-			if(mirando != 'derecha'){
-				mirando = 'derecha';
-			}
+		if (cursores.right.isDown) {
+			nave.position.x += 3;
 		}else if(cursores.left.isDown){
-			//pajaro.position.x -= 1;
-			persona.position.x -= 1;
-			persona.animations.play('izquierda');
-			if(mirando != 'izquierda'){
-				mirando = 'izquierda';
-			}
-		}else if(cursores.up.isDown){
-			//pajaro.position.y -= 1;
-			persona.position.y -= 1;
-			persona.animations.play('arriba');
-			if(mirando != 'arriba'){
-				mirando = 'arriba';
-			}
-		}else if(cursores.down.isDown){
-			//pajaro.position.y += 1;
-			persona.position.y += 1;
-			persona.animations.play('abajo');
-			if(mirando != 'abajo'){
-				mirando = 'abajo';
-			}
-		}else{
-			if(mirando != 'espera'){
-				persona.animations.stop();
-			}
-			mirando = 'espera';
+			nave.position.x -= 3;
 		}
+
+		var bala;
+		if(botonDisparo.isDown){
+			if(juego.time.now > tiempoBala){
+				bala = balas.getFirstExists(false);
+			}
+			if (bala) {
+				bala.reset(nave.x,nave.y);
+				bala.body.velocity.y = -300;
+				tiempoBala = juego.time.now + 100;
+			}
+		}
+
 	}
 };
 
